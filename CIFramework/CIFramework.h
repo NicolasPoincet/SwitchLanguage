e2078b0649d372ee9605e1c26e4e6482b4d6a07f
Nicolas <nicolas.poincet@gmail.com>
2018-01-18 16:19:18 +0100

    First commit

---
+++ CIFramework/CIFramework.h
@@ -0,0 +1,19 @@
+//
+//  CIFramework.h
+//  CIFramework
+//
+//  Created by Nicolas POINCET on 18/01/2018.
+//  Copyright © 2018 Nicolas POINCET. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CIFramework.
+FOUNDATION_EXPORT double CIFrameworkVersionNumber;
+
+//! Project version string for CIFramework.
+FOUNDATION_EXPORT const unsigned char CIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CIFramework/PublicHeader.h>
+
+
