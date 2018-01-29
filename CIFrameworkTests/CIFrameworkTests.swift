d28ae60f76aca4ca95c124320af7a4fe63f4cc12
Nicolas <nicolas.poincet@gmail.com>
2018-01-29 22:36:53 +0100

    setup unit tests

--- CIFrameworkTests/CIFrameworkTests.swift
+++ CIFrameworkTests/CIFrameworkTests.swift
@@ -21,16 +21,13 @@ class CIFrameworkTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testGetAllLanguages(){
+        
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testgetCurrentLanguage() {
+        let currentLanguage = Language.getCurrentLanguage()
+        XCTAssertEqual(currentLanguage, "en")
     }
     
 }
